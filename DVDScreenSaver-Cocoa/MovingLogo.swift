862930650684d8c8e08b5a20acc0c9ca2cf0c6f6
Kevin Wojniak <kainjow@users.noreply.github.com>
2019-03-23 23:49:10 -0700

    Cleanup

--- DVDScreenSaver-Cocoa/MovingLogo.swift
+++ DVDScreenSaver-Cocoa/MovingLogo.swift
@@ -1,11 +1,3 @@
-//
-//  MovingLogo.swift
-//  DVDScreenSaver-TestApp
-//
-//  Created by Kevin Wojniak on 3/23/19.
-//  Copyright © 2019 Kevin Wojniak. All rights reserved.
-//
-
 import Cocoa
 
 typealias RectDbl = NSRect
